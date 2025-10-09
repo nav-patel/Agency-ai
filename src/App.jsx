6af7035d3dfbfcb60e959037c09ca8dad797d2a4
Nav0009 <navdeeppatel319@gmail.com>
2025-10-09 20:23:52 +0530

    createad and mounted TrustedBy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import TrustedBy from "./components/TrustedBy"
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
     <div className='dark:bg-black relative'>
     <Navbar theme={theme} setTheme={setTheme} />
     <Hero/>
+    <TrustedBy/> 
     
     </div>
   )
