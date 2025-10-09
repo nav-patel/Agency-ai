5a3582ed21d6737c46a9538dea2e702917c84bc9
Nav0009 <navdeeppatel319@gmail.com>
2025-10-09 22:38:27 +0530

    created ServieCard component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import assets from '../assets/assets/assets'
 import Title from './Title'
+import ServiceCard from './ServiceCard'
 
 const Services = () => {
 
@@ -34,6 +35,12 @@ const Services = () => {
         <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
 
         <Title title='how can we help ?' desc='From strategy to execution we craft digital solutions that move your business forward.'/>
+
+        <div className='flex flex-col md:grid grid-cols-2'>
+            {servicesData.map((service , index)=>(
+                <ServiceCard key={index} service={service} index={index}/>
+            ))}
+        </div>
 
     </div>
   )
