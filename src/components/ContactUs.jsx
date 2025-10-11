4afaaeb90f16cc44744782dff11f88acf90c94b6
Nav0009 <navdeeppatel319@gmail.com>
2025-10-11 11:01:22 +0530

    Added form section in the ContactUs component

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from './Title'
+import assets from '../assets/assets/assets'
 
 const ContactUs = () => {
   return (
@@ -7,7 +8,36 @@ const ContactUs = () => {
 
        <Title title='Reach out to us' desc='From strategy to execution , we craft digital solutions that move your business forward.'/>
 
-       <form >
+       <form className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full '>
+
+        <div>
+            <p className='mb-2 text-sm font-medium'>Your name</p>
+
+            <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                <img src={assets.person_icon} alt="" />
+                <input type="text" placeholder='Enter your name'className='w-full p-3 text-sm outline-none ' required/>
+            </div>
+
+        </div>
+
+        <div>
+            <p className='mb-2 text-sm font-medium'>Email id</p>
+
+            <div className='flex pl-3 rounded-lg border border-gray-300 dark:border-gray-600'>
+                <img src={assets.email_icon} alt="" />
+                <input type="email" placeholder='Enter your name'className='w-full p-3 text-sm outline-none' required/>
+            </div>
+
+        </div>
+
+        <div className='sm:col-span-2'>
+            <p className='mb-2 text-sm font-medium'>Message</p>
+            <textarea rows={8} placeholder='Enter your message' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600'/>
+        </div>
+
+        <button type="submit" className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all '>
+            Submit <img src={assets.arrow_icon} alt="" className='w-4'/>
+        </button>
 
        </form>
 
