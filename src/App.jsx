c0eeaef1407aeb02f31405eb605811bf28374970
Nav0009 <navdeeppatel319@gmail.com>
2025-10-11 10:35:02 +0530

    created and mounted ContactUs component in the App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TrustedBy from "./components/TrustedBy"
 import Services from "./components/Services"
 import OurWork from "./components/OurWork"
 import Teams from "./components/Teams"
+import ContactUs from "./components/ContactUs"
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
     <Services/>
     <OurWork/>
     <Teams/>
+    <ContactUs/>
     
     </div>
   )
